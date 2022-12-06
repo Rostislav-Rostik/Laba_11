c89be3f0bb98056819f2d64243337b560fa27a26
rosti <rosti@ROSTISLAV>
2022-12-06 14:19:43 +0200

    Добавьте файлы проекта.

---
+++ Task3/Task4.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+internal class Task4
+{
+
+    public interface IHours
+    {
+        public int Hours();
+        public string Name();
+    }
+
+    public class StandartEmployee : IHours
+    {
+        private string name;
+        private int hours;
+
+        public StandartEmployee(string name)
+        {
+            this.name = name;
+            hours = 40;
+        }
+
+        public int Hours()
+        {
+            return hours;
+        }
+
+        public string Name()
+        {
+            return name;
+        }
+    }
+
+    public class PartTimeEmployee : IHours
+    {
+        private string name;
+        private int hours;
+
+        public PartTimeEmployee(string name)
+        {
+            this.name = name;
+            hours = 20;
+        }
+
+        public int Hours()
+        {
+            return hours;
+        }
+
+        public string Name()
+        {
+            return name;
+        }
+    }
+
+    public class Job
+    {
+        private string name;
+        private int neededHour;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int NeededHour
+        {
+            get { return neededHour; }
+            set { neededHour = value; }
+        }
+
+        private IHours employee;
+
+        public Job(string name, int neededHour, IHours employee)
+        {
+            this.employee = employee;
+            this.neededHour = neededHour;
+            this.name = name;
+        }
+
+        public int PassWeek()
+        {
+            if (neededHour - employee.Hours() <= 0 || neededHour <= 0)
+            {
+                return 1;
+            }
+            neededHour -= employee.Hours();
+            return 0;
+        }
+
+        public void Status()
+        {
+            Console.WriteLine($"Job: {name} \nHours Remaining: {neededHour}");
+        }
+    }
+
+    static void Main()
+    {
+        List<IHours> hours = new List<IHours>();
+        List<Job> jobs = new List<Job>();
+
+        while (true)
+        {
+            var temp = Console.ReadLine().Split();
+
+            if (temp[0] == "END" || temp[0] == "End")
+            {
+                break;
+            }
+            else
+            {
+                if (temp[0] == "StandartEmployee")
+                {
+                    StandartEmployee parttemp = new StandartEmployee(temp[1]);
+                    hours.Add(parttemp);
+                }
+
+                else if (temp[0] == "PartTimeEmployee")
+                {
+                    PartTimeEmployee parttemp = new PartTimeEmployee(temp[1]);
+                    hours.Add(parttemp);
+                }
+
+
+                else if (temp[0] == "Job")
+                {
+                    foreach (IHours item in hours)
+                    {
+                        if (item.Name() == temp[3])
+                        {
+                            Job job = new Job(temp[1], int.Parse(temp[2]), item);
+                            jobs.Add(job);
+
+                            break;
+                        }
+                    }
+                }
+                else if (temp[0] == "Status")
+                {
+
+                    foreach (Job item in jobs)
+                    {
+                        item.Status();
+                    }
+                }
+                else if (temp[0] == "PassWeek")
+                {
+                    foreach (Job item in jobs)
+                    {
+                        if (item.PassWeek() == 1)
+                        {
+                            Console.WriteLine($"Job {item.Name} done");
+                            jobs.Remove(item);
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
+
---
+++ Task5/Task5.cs
@@ -0,0 +1,164 @@
+﻿using System.Diagnostics.Metrics;
+
+internal class Task2
+{
+    public delegate void Reaction();
+
+    public class King
+    {
+        public string name;
+        public event Reaction action;
+
+        public King(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public void GetReaction()
+        {
+            Console.WriteLine($"King {name} is under attack!");
+        }
+    }
+
+    public class Footman
+    {
+        private string name;
+        private int health = 2;
+
+        public Footman(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+        }
+
+        public void Kill()
+        {
+            health--;
+        }
+
+        public void GetReaction()
+        {
+            Console.WriteLine($"Footman {name} is panicking!");
+        }
+    }
+
+    public class Royal_Guard
+    {
+        private string name;
+        private int health = 3;
+
+        public Royal_Guard(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Health 
+        {
+            get { return health; }
+            
+        }
+
+        public void Kill()
+        {
+            health--;
+        }
+
+        public void GetReaction()
+        {
+            Console.WriteLine($"Royal Guard {name} is defending!");
+        }
+    }
+
+    static void Main()
+    {
+        var temp = Console.ReadLine().Split();
+        King king = new King(temp[0]);
+
+        List<Royal_Guard> royal_Guards = new List<Royal_Guard>();
+        List<Footman> footman = new List<Footman>();
+
+        temp = Console.ReadLine().Split();
+
+        foreach (string item in temp)
+        {
+            Royal_Guard temp_item = new Royal_Guard(item);
+            royal_Guards.Add(temp_item);
+
+        }
+
+        temp = Console.ReadLine().Split();
+
+        foreach (string item in temp)
+        {
+            Footman temp_item = new Footman(item);
+            footman.Add(temp_item);
+        }
+
+        while (true)
+        {
+            temp = Console.ReadLine().Split();
+            if (temp[0] == "END" || temp[0] == "End")
+            {
+                break;
+            }
+            else if (temp[0] == "Attack")
+            {
+                king.GetReaction();
+                foreach (Royal_Guard temp_item in royal_Guards)
+                {
+                    temp_item.GetReaction();
+                }
+                foreach (Footman temp_item in footman)
+                {
+                    temp_item.GetReaction();
+                }
+            }
+            else if (temp[0] == "Kill")
+            {
+                for (int i = 0; i != royal_Guards.Count; i++)
+                {
+                    if (royal_Guards[i].Name == temp[1])
+                    {
+                        royal_Guards[i].Kill();
+                        if (royal_Guards[i].Health <= 0)
+                        {
+                            royal_Guards.RemoveAt(i);
+                        }
+                        
+                    }
+                }
+                for (int i = 0; i != footman.Count; i++)
+                {
+                    if (footman[i].Name == temp[1])
+                    {
+                        footman[i].Kill();
+                        if (footman[i].Health <= 0)
+                        {
+                            footman.RemoveAt(i);
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
